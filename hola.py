c0cd2fea356c3d631691e7d574f03470d35a612e
domjosepy <josdo9717@gmail.com>
2023-10-26 20:14:25 -0300

    ejercicio 2 terminado

--- hola.py
+++ hola.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 def hola_mundo():
     return "<p>Hola, Mundo!</p>"
 
+@app.route("/titulo-2")
+def titulo2():
+    return "<h2>Mi mensaje personalizado.</h2>"
+
+@app.route("/parrafo")
+def parrafo():
+    return "<p>ejercicio 2 terminado</p>"
+
+
